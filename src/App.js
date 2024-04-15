19749cc2d7876e9b5a96580a3c231c713fa9831b
AbdimalikAbukar <n01630627@humbermail.ca>
2024-04-15 13:58:02 -0400

    final pls

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Home from './Home';
 import NobelPrizeInfo from './NobelPrizeInfo';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
